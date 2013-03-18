88fcc4ae932d164a8b0c9e34de6bd53b6fce8ea5
51isoft <yichaonet@gmail.com>
2013-03-18 17:45:45 +0800

    Update

--- content/first_post.md
+++ content/first_post.md
@@ -1,6 +1,6 @@
 Title: 第一篇文章
 Date: 2013-03-17 14:20
-Tags:
+Tags: test
 Category: Blogs
 Slug: first-article
 Author: 51isoft
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,9 +3,11 @@
 
 AUTHOR = u'51isoft'
 SITENAME = u"51isoft's Blog"
-SITEURL = ''
-OUTPUT_PATH = '51isoft.github.com/'
+SITEURL = 'http://51isoft.github.com'
+OUTPUT_PATH = '../51isoft.github.com/'
+FEED_DOMAIN = 'http://51isoft.github.com'
 THEME = 'theme_bootstrap'
+DISQUS_SITENAME = '51isoftsblog'
 
 TIMEZONE = 'Asia/Shanghai'
 
@@ -17,6 +19,8 @@ LINKS =  (('BNUOJ', 'http://www.bnuoj.com/'),)
 # Social widget
 SOCIAL = (('Twitter', 'http://twitter.com/51isoft'),
           ('Facebook', 'http://facebook.com/51isoft'),
-          ('Linkedin', 'http://cn.linkedin.com/pub/chao-yi/49/544/42b'),)
+          ('Linkedin', 'http://cn.linkedin.com/pub/chao-yi/49/544/42b'),
+          ('Weibo', 'http://weibo.com/51isoft'),
+          ('Github', 'http://github.com/51isoft'),)
 
 DEFAULT_PAGINATION = 10
